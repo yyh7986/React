c45b1cf5412510a2ac3cf440a102025d72a4b8b7
yyh7986 <yyh7986@gmail.com>
2024-07-09 18:13:12 +0900

    feat: useEffect에 [ ] 추가
    누락시 지속된 호출로 too many connection 에러

--- client11/src/Component/Join.js
+++ client11/src/Component/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../css/join.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -10,63 +10,87 @@ function Join(props) {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   // location.href 와 비슷한 기능의 함수를 생성
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const [userInfo, setUserInfo] = useState({});
 
   const onSubmit = () => {
-    setUserInfo({userid, pwd, name, phone, email});
+    setUserInfo({ userid, pwd, name, phone, email });
     props.contentList.push(userInfo);
-    axios.post("/api/join", {userid, pwd, name, phone, email})
-    .then(()=>{
-      alert("회원가입이 완료되었습니다.")
-    })
-    .catch((err)=>{
-      console.error(err);
-    });
-    
+    axios
+      .post("/api/join", { userid, pwd, name, phone, email })
+      .then(() => {
+        alert("회원가입이 완료되었습니다.");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
     setuserid("");
     setPwd("");
     setName("");
     setPhone("");
     setEmail("");
-  }
+  };
   return (
     <div className="container">
       <div className="field">
         <label>아이디</label>
-        <input type="text" value={userid} onChange={(e)=>{
-          setuserid(e.currentTarget.value);
-        }}/>
+        <input
+          type="text"
+          value={userid}
+          onChange={(e) => {
+            setuserid(e.currentTarget.value);
+          }}
+        />
       </div>
       <div className="field">
         <label>비밀번호</label>
-        <input type="password" value={pwd} onChange={(e)=>{
-          setPwd(e.currentTarget.value);
-        }}/>
+        <input
+          type="password"
+          value={pwd}
+          onChange={(e) => {
+            setPwd(e.currentTarget.value);
+          }}
+        />
       </div>
       <div className="field">
         <label>이름</label>
-        <input type="text" value={name} onChange={(e)=>{
-          setName(e.currentTarget.value);
-        }}/>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.currentTarget.value);
+          }}
+        />
       </div>
       <div className="field">
         <label>전화번호</label>
-        <input type="text" value={phone} onChange={(e)=>{
-          setPhone(e.currentTarget.value);
-        }}/>
+        <input
+          type="text"
+          value={phone}
+          onChange={(e) => {
+            setPhone(e.currentTarget.value);
+          }}
+        />
       </div>
       <div className="field">
         <label>이메일</label>
-        <input type="text" value={email} onChange={(e)=>{
-          setEmail(e.currentTarget.value);
-        }}/>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.currentTarget.value);
+          }}
+        />
       </div>
       <div className="btns" style={{ display: "flex", width: "100%" }}>
-        <button style={{ flex: "1", height: "50px" }} onClick={() => {
-          onSubmit();
-        }}>
+        <button
+          style={{ flex: "1", height: "50px" }}
+          onClick={() => {
+            onSubmit();
+          }}
+        >
           제출
         </button>
       </div>
--- client11/src/Component/List.js
+++ client11/src/Component/List.js
@@ -1,31 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function List(props) {
   const [members, setMembers] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     // 멤버를 조회해서 members 변수에 저장하고 아래 return 에서 전송받은 데이터를 화면에 출력
-    axios.post("/api/getMember")
-    .then((res)=>{
-      console.log("res.data : ", res.data);
-      setMembers([...res.data]);
-    })
-    .catch((err)=>{
-      console.error(err);
-    });
-  })
+    axios
+      .post("/api/getMember")
+      .then((res) => {
+        console.log("res.data : ", res.data);
+        setMembers([...res.data]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return (
     <div>
       {/* props로 전달된 배열을 출력 */}
-      {
-        members.map((list, idx)=>{
-          // return (<div>{JSON.stringify(list)}</div>)
-          return (<div>{JSON.stringify(list)}</div>)
-        })
-      }
+      {members.map((list, idx) => {
+        // return (<div>{JSON.stringify(list)}</div>)
+        return <div>{JSON.stringify(list)}</div>;
+      })}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
